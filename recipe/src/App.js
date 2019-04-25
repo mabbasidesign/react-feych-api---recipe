697d545118c91b1ce75e6fca55574045e623bb89
unknown <mabbasidesign@yahoo.com>
2019-04-25 14:03:18 -0400

    add ingrediants

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -9,21 +9,35 @@ const APP_ID = "6348b915";
 const APP_KEY = "f2f9356790918ea18bcac41a180af131";
 
 const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState('');
+const [query, setQuery] = useState('chicken');
+
 
 useEffect(() => {
     getRecipes();
-}, []);
+}, [query]);
 
 const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
     const data = await response.json();
     setRecipes(data.hits);
 }
 
+const updateSearch = (e) => {
+    setSearch(e.target.value);
+    // setQuery(search);
+}
+
+const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+}
+
     return ( 
         <div className="App">
-            <form className="search-form">
-                <input className="search-bar" type="text" />
+            <form onSubmit={getSearch} className="search-form">
+                <input className="search-bar" type="text" value={search} onChange={updateSearch} />
                 <button 
                     className="search-button"
                     type="submit">
@@ -32,10 +46,11 @@ const getRecipes = async () => {
             </form>
             {recipes.map(recipe => (
                 <Recipe
-                    key={recipe.recipe.key}
+                    key="id"
                     title={recipe.recipe.label}
                     calories={recipe.recipe.calories}
                     image={recipe.recipe.image}
+                    ingrediants={recipe.recipe.ingrediants}
                 />
             ))}
         </div>
--- recipe/src/recipe.jsx
+++ recipe/src/recipe.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingrediants}) => {
     return (
         <div>
             <h1>{title}</h1>
+            {/* <ol>
+                {ingrediants.map(ingrediant => (
+                    <li>{ingrediant.text}</li>
+                ))}
+            </ol> */}
             <h2>{calories}</h2>
             <img src={image} alt=""/>
         </div>
