ace3bba45fa82e42479a5dc033a26824b5424a44
unknown <mabbasidesign@yahoo.com>
2019-04-25 10:22:23 -0400

    add api key

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const APP_ID = "6348b915";
+    const APP_KEY = "f2f9356790918ea18bcac41a180af131";
+    const exampleReq = `curl "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"`
+
+    return ( 
+        <div>
+            
+        </div>
+     );
 }
 
 export default App;
