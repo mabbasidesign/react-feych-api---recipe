3c3742cb2cea78ef668f3b3cb7dd5db091e78dcb
unknown <mabbasidesign@yahoo.com>
2019-04-25 11:45:47 -0400

    get recipes

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 
 const App = () => {
 
-    const APP_ID = "6348b915";
-    const APP_KEY = "f2f9356790918ea18bcac41a180af131";
-    const exampleReq = `curl "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"`
+const APP_ID = "6348b915";
+const APP_KEY = "f2f9356790918ea18bcac41a180af131";
+
+const [counter, setCounter] = useState(0);
+const [recipes, setRecipes] = useState([]);
+
+useEffect(() => {
+    getRecipes();
+}, []);
+
+const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+    const data = await response.json();
+    console.log(data.hits);
+}
 
     return ( 
         <div className="App">
             <form className="search-form">
                 <input className="search-bar" type="text" />
-                <button className="search-button" type="submit">Search</button>
+                <button 
+                    className="search-button"
+                    type="submit">
+                Search
+                </button>
+                <h1 onClick={() => setCounter(counter + 1)}> {counter} </h1>
             </form>
         </div>
      );
