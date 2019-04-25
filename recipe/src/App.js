160d9403467a4f72fd98b16e7b847ebf533a2a98
unknown <mabbasidesign@yahoo.com>
2019-04-25 10:27:49 -0400

    add form

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -9,8 +9,11 @@ const App = () => {
     const exampleReq = `curl "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"`
 
     return ( 
-        <div>
-            
+        <div className="App">
+            <form className="search-form">
+                <input className="search-bar" type="text" />
+                <button className="search-button" type="submit">Search</button>
+            </form>
         </div>
      );
 }
